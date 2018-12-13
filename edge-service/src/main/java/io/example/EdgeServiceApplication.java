98336bfd445f9c975beddf502556a17365000eab
kbastani <2095770+kbastani@users.noreply.github.com>
2018-12-12 17:37:38 -0800

    #7 Replace Spring Cloud Zuul with Reactive Spring Cloud Gateway

--- edge-service/src/main/java/io/example/EdgeServiceApplication.java
+++ edge-service/src/main/java/io/example/EdgeServiceApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
+@EnableWebFlux
 public class EdgeServiceApplication {
 
     public static void main(String[] args) {
