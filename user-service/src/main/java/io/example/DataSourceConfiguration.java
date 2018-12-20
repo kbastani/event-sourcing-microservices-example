1a3e551f267e4227cfdd8029eeeaed8bea6a7680
Paul Czarkowski <username.taken@gmail.com>
2018-12-20 00:05:12 -0600

    update microservices to support kubernetes
    This brings in the spring cloud kubernetes dependencies and sets
    up a new kubernetes profile for each application that can be used
    to allow for kube discovery and ribbon.
    
    Signed-off-by: Paul Czarkowski <username.taken@gmail.com>

--- user-service/src/main/java/io/example/DataSourceConfiguration.java
+++ user-service/src/main/java/io/example/DataSourceConfiguration.java
@@ -27,7 +27,7 @@ import javax.sql.DataSource;
  */
 @Configuration
 @EnableR2dbcRepositories
-@Profile({"docker", "development"})
+@Profile({"kubernetes", "docker", "development"})
 public class DataSourceConfiguration extends AbstractR2dbcConfiguration {
 
     @Value("${postgres.host}")
