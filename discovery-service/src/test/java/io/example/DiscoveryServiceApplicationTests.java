9976a3f1c5708c8ead4a48f66e77b97760a8ab53
kbastani <kb@socialmoon.com>
2022-11-26 11:01:40 -0500

    Fixing build failure for tests

--- discovery-service/src/test/java/io/example/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/io/example/DiscoveryServiceApplicationTests.java
@@ -1,18 +1,21 @@
 package io.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public class DiscoveryServiceApplicationTests {
 
-    @Test
-    public void testsVoid() {
-        Assert.state(true, "The tests for this service are turned off for this example.");
-    }
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    public void contextLoads() {
+        Assert.notNull(applicationContext, "The application context should not be null");
+    }
 }
